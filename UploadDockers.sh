d30703f202030734be33410fd55621c6dd7ac12a
OllieGYoung <118399179+OllieGYoung@users.noreply.github.com>
2024-10-18 14:22:17 +0100

    Update UploadDockers.sh

--- UploadDockers.sh
+++ UploadDockers.sh
@@ -4,8 +4,17 @@
 docker save biocontainers/trimmomatic:v0.38dfsg-1-deb_cv1 > trimmomatic:v0.38dfsg-1-deb_cv1.tar
 docker save staphb/fastqc:0.12.1 > fastqc_0.12.1.tar
 docker save multiqc/multiqc:v1.25.1 > multiqc:v1.25.1.tar
+docker save swglh/samtools:1.18 > samtools:1.18.tar
+docker save swglh/bwamem2:v2.2.1 > bwamem2:v2.2.1.tar
+docker save dancooke/octopus:invitae--eae1ab48_0 > octopus:invitae--eae1ab48_0.tar
 
 # Upload the Docker image tar file to DNAnexus
-dx upload trimmomatic:v0.38dfsg-1-deb_cv1.tar
-dx upload fastqc_0.12.1.tar
-dx upload multiqc:v1.25.1.tar
+PATH_docker_base=$"Diagnostic Sequencing:/outputs/Exeter_WDL_pipeline/resources/DockerImages"
+
+dx upload trimmomatic:v0.38dfsg-1-deb_cv1.tar --path $PATH_docker
+dx upload fastqc_0.12.1.tar --path $PATH_docker
+dx upload multiqc:v1.25.1.tar --path $PATH_docker
+dx upload samtools:1.18.tar --path $PATH_docker
+dx upload bwamem2:v2.2.1.tar --path $PATH_docker
+dx upload octopus:invitae--eae1ab48_0.tar --path $PATH_docker
+
